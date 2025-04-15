352a955d62ab115c72105097c59bd45e4c553c0c
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-04-15 15:29:14 -0400

    aula-02

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,31 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
+
+const times = [
+  'Programação',
+  'Educação Física',
+  'Gastronomia'
+]
+
+const aoSalvar = (evento) => {
+  evento.preventDefault()
+  console.log('Form foi submetido')
+}
 
 const Formulario = () => {
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   );
