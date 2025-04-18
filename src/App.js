c9af2330814e14fd5dd6489cc4d1aa5ede784525
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2025-04-18 08:53:46 -0400

    aula-05

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
   
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
+        setColaboradores([...colaboradores, colaborador])
   }
 
   return (
